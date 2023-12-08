72298ebc84173ebc5e79d72aad575c341ba7203d
JoseGarPe <jhosuegarciastarkand@gmail.com>
2023-12-07 19:38:54 -0600

    Update index.php

--- application/public/index.php
+++ application/public/index.php
@@ -70,7 +70,7 @@ register_shutdown_function($shutdownHandler);
 
 // Add Routing Middleware
 //$app->addRoutingMiddleware();
-$app->setBasePath('/application');
+$app->setBasePath('PeerPromote_Backend/application');
 
 // Add Body Parsing Middleware
 //$app->addBodyParsingMiddleware();
