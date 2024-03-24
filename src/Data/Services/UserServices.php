2ef2d8f08310a5db6adb91b519344dee523d77e5
David Melara <davidsal1995@gmail.com>
2024-03-24 00:30:18 -0600

    Create username service

--- src/Data/Services/UserServices.php
+++ src/Data/Services/UserServices.php
@@ -1,11 +1,16 @@
 <?php
 namespace Data\Services;
+
 use Data\Dependencies\DependencyManager;
 
 class UserServices
 {
-    public static function queryUsernameData()
+    public static function queryUsernameData(string $username)
     {
         $db = DependencyManager::getDatabase();
+        $query = "SELECT * FROM Users WHERE Username=:username";
+        
+        $result = $db->query($query, ['username' => $username])->fetch();
+        return $result;
     }
 }
--- src/Domain/UseCases/UserUseCases.php
+++ src/Domain/UseCases/UserUseCases.php
@@ -1,12 +1,14 @@
 <?php
 namespace Domain\UseCases;
 
+use Data\Services\UserServices;
+
 class UserUseCases
 {
     public static function isUsernameAvailableUseCase(string $username): bool
     {
-        // Logica para Medoo
-        return true;   
+        $userData = UserServices::queryUsernameData($username);
+        return ($userData === false);
     }
 
     public static function isEmailAvailableUseCase(string $email): bool
