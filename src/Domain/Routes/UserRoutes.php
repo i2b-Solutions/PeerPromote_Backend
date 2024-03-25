114d4a5e1d1a2c0a409591d9689714439763df6e
David Melara <davidsal1995@gmail.com>
2024-03-24 23:12:07 -0600

    Fix error

--- src/Domain/Routes/UserRoutes.php
+++ src/Domain/Routes/UserRoutes.php
@@ -16,72 +16,83 @@ class UserRoutes
     {
         $app->group('/user', function (Group $group) {
             $group->post('/check_username', function (Request $request, Response $response) {
-                $postData = $request->getParsedBody();
-                $username = $postData['user'] ?? null;
-
-                if ($isEmptyResponse = DomainHelpers::generateEmptyResponseData([$username])) {
-                    return DomainHelpers::createJsonResponse($isEmptyResponse, $response);
-                }
-
                 try {
+                    $postData = $request->getParsedBody();
+                    $username = $postData['user'] ?? null;
+
+                    if ($isEmptyResponse = DomainHelpers::generateEmptyResponseData([$username])) {
+                        return DomainHelpers::createJsonResponse($isEmptyResponse, $response);
+                    }
+
                     $checkResult = UserController::isUsernameAvailable($username);
                     $responseData = DomainHelpers::generateOkResponseData($checkResult);
                     return DomainHelpers::createJsonResponse($responseData, $response);
                 } catch (\Throwable $th) {
-                    return DomainHelpers::generateErrorResponseData($th, $response);
+                    return DomainHelpers::createJsonResponse(
+                        DomainHelpers::generateErrorResponseData($th, $response),
+                        $response
+                    );
                 }
             });
 
             $group->post('/check_email', function (Request $request, Response $response) {
-                $postData = $request->getParsedBody();
-                $email = $postData['email'] ?? null;
-
-                if ($isEmptyResponse = DomainHelpers::generateEmptyResponseData([$email])) {
-                    return DomainHelpers::createJsonResponse($isEmptyResponse, $response);
-                }
-
                 try {
+                    $postData = $request->getParsedBody();
+                    $email = $postData['email'] ?? null;
+
+                    if ($isEmptyResponse = DomainHelpers::generateEmptyResponseData([$email])) {
+                        return DomainHelpers::createJsonResponse($isEmptyResponse, $response);
+                    }
+
+
                     $checkResult = UserController::isEmailAvailable($email);
                     $responseData = DomainHelpers::generateOkResponseData($checkResult);
                     return DomainHelpers::createJsonResponse($responseData, $response);
                 } catch (\Throwable $th) {
-                    return DomainHelpers::generateErrorResponseData($th, $response);
+                    return DomainHelpers::createJsonResponse(
+                        DomainHelpers::generateErrorResponseData($th, $response),
+                        $response
+                    );
                 }
             });
 
             $group->post('/register', function (Request $request, Response $response) {
-                $postData = $request->getParsedBody();
-                $uploadedFiles = $request->getUploadedFiles();
-
-                $selfieLocation = UserHelpers::generateSelfieLocation();
-
-                $user = new RegisterUserRequestData(
-                    $postData['user'] ?? null,
-                    $postData['pass'] ?? null,
-                    $postData['countryId'] ?? null,
-                    $postData['isCompany'] ?? null,
-                    $postData['cityId'] ?? null,
-                    $postData['languages'] ?? null,
-                    $postData['birthdate'] ?? null,
-                    $postData['email'] ?? null,
-                    $postData['area'] ?? null,
-                    $postData['phone'] ?? null,
-                    $uploadedFiles['selfie'] ?? null,
-                    $selfieLocation
-                );
-
-                if ($isEmptyResponse = DomainHelpers::generateEmptyResponseData($user->toArray())) {
-                    return DomainHelpers::createJsonResponse($isEmptyResponse, $response);
-                }
-
                 try {
+                    $postData = $request->getParsedBody();
+                    $uploadedFiles = $request->getUploadedFiles();
+
+                    $selfieLocation = UserHelpers::generateSelfieLocation();
+
+                    $user = new RegisterUserRequestData(
+                        $postData['user'] ?? null,
+                        $postData['pass'] ?? null,
+                        $postData['countryId'] ?? null,
+                        $postData['isCompany'] ?? null,
+                        $postData['cityId'] ?? null,
+                        $postData['languages'] ?? null,
+                        $postData['birthdate'] ?? null,
+                        $postData['email'] ?? null,
+                        $postData['area'] ?? null,
+                        $postData['phone'] ?? null,
+                        $uploadedFiles['selfie'] ?? null,
+                        $selfieLocation
+                    );
+
+                    if ($isEmptyResponse = DomainHelpers::generateEmptyResponseData($user->toArray())) {
+                        return DomainHelpers::createJsonResponse($isEmptyResponse, $response);
+                    }
+
+
                     $registerResult = UserController::registerUser($user);
                     $responseData = DomainHelpers::generateOkResponseData($registerResult);
                     $userId = $registerResult->userId;
                     UserHelpers::saveSelfie($selfieLocation, $userId, $user->selfie);
                     return DomainHelpers::createJsonResponse($responseData, $response);
                 } catch (\Throwable $th) {
-                    return DomainHelpers::generateErrorResponseData($th, $response);
+                    return DomainHelpers::createJsonResponse(
+                        DomainHelpers::generateErrorResponseData($th, $response),
+                        $response
+                    );
                 }
             });
         });
